dce185b34bb9280008a5aea551f94e6487ad862c
Vincent van Adrighem <Vincent.van.Adrighem@connectis.nl>
2026-04-06 12:03:13 +0200

    feat: bump version to 2.0.0 and add release-please workflow

--- plugin.py
+++ plugin.py
@@ -1,15 +1,16 @@
 # pp-manager - PythonPlugin Manager
 #
-# Author: adrighem, 2018
+# Author: ycahome, 2018
+# Author: adrighem, 2026
 #
 #  Since (2018-02-23): Initial Version
 #
 
 
 """
-<plugin key="PP-MANAGER" name="Python Plugin Manager" author="adrighem" version="1.5.47" externallink="https://www.domoticz.com/forum/viewtopic.php?f=65&t=22339">
+<plugin key="PP-MANAGER" name="Python Plugin Manager" author="adrighem" version="2.0.0" externallink="https://forum.domoticz.com/viewtopic.php?t=22339">
     <description>
-		<h2>Python Plugin Manager 1.5.47</h2><br/>
+		<h2>Python Plugin Manager 2.0.0</h2><br/>
 		<h3>Features</h3>
 		<ul style="list-style-type:square">
 			<li>Install plugins</li>
