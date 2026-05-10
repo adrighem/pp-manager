b48c5172dc8bee841415e6b9edc27411e20b93e6
Vincent van Adrighem <Vincent.van.Adrighem@connectis.nl>
2026-05-10 14:21:22 +0200

    fix: resolve XML encoding issue in plugin generator

--- .github/scripts/generate_plugin.py
+++ .github/scripts/generate_plugin.py
@@ -14,30 +14,30 @@ def generate_plugin():
 
 
 """
-<plugin key="PP-MANAGER" name="Python Plugin Manager" author="adrighem" version="2.0.0" externallink="https://www.domoticz.com/forum/viewtopic.php?f=65&t=22339"> &lt;!-- x-release-please-version --&gt;
+<plugin key="PP-MANAGER" name="Python Plugin Manager" author="adrighem" version="2.0.0" externallink="https://www.domoticz.com/forum/viewtopic.php?f=65&t=22339"> <!-- x-release-please-version -->
     <description>
-        &lt;h2&gt;Python Plugin Manager&lt;/h2&gt;&lt;br/
-        This plugin manages other Domoticz Python plugins.&lt;br/&gt;&lt;br/
-        &lt;b&gt;Usage:&lt;/b&gt;&lt;br/
-        1. Add this hardware to Domoticz.&lt;br/
-        2. Navigate to &lt;b&gt;Custom&lt;/b&gt; -&gt; &lt;b&gt;Plugin Manager&lt;/b&gt; in the top menu to manage your plugins.
-    &lt;/description&gt;
-    &lt;params&gt;
-        &lt;param field="Mode4" label="Auto Update" width="175px"&gt;
-            &lt;options&gt;
-                &lt;option label="All" value="All"/>
-                &lt;option label="All (NotifyOnly)" value="AllNotify" default="true"/>
-                &lt;option label="None" value="None"/>
-            &lt;/options&gt;
-        &lt;/param&gt;
-        &lt;param field="Mode6" label="Debug" width="75px"&gt;
-            &lt;options&gt;
-                &lt;option label="True" value="Debug"/>
-                &lt;option label="False" value="Normal" default="true" /&gt;
-            &lt;/options&gt;
-        &lt;/param&gt;
-    &lt;/params&gt;
-&lt;/plugin&gt;
+        <h2>Python Plugin Manager</h2><br/>
+        This plugin manages other Domoticz Python plugins.<br/><br/>
+        <b>Usage:</b><br/>
+        1. Add this hardware to Domoticz.<br/>
+        2. Navigate to <b>Custom</b> -> <b>Plugin Manager</b> in the top menu to manage your plugins.
+    </description>
+    <params>
+        <param field="Mode4" label="Auto Update" width="175px">
+            <options>
+                <option label="All" value="All"/>
+                <option label="All (NotifyOnly)" value="AllNotify" default="true"/>
+                <option label="None" value="None"/>
+            </options>
+        </param>
+        <param field="Mode6" label="Debug" width="75px">
+            <options>
+                <option label="True" value="Debug"/>
+                <option label="False" value="Normal" default="true" />
+            </options>
+        </param>
+    </params>
+</plugin>
 """
 '''
 
